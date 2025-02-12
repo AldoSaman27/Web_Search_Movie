dd2ec3cc00c75be9cb78541a3909b46b280e7195
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2025-02-12 09:14:33 +0700

    refactor: Restructure 'App.js' file.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+// Style
 import './App.css';
+
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components Movie
 import Movie from './components/Movie';
+
+// Components Header
 import Header from './components/Header';
+
+// Components NavigationBar
 import NavigationBar from './components/NavigationBar';
+
+// Components Footer
 import Footer from './components/Footer';
 
 
 function App() {
-  return (
-    <div>
-      <NavigationBar/>
-      <Header/>
-      <Movie/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <>
+            {/* NavigationBar */}
+            <NavigationBar/>
+
+            {/* Header */}
+            <Header/>
+
+            {/* Movie */}
+            <Movie/>
+
+            {/* Footer */}
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
