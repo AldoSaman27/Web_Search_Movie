c56af2e658349f9a424a1c139718c6496c62ff3d
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2025-02-12 09:10:41 +0700

    refactor: Restructure 'Movie.js' file.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,38 +1,43 @@
+// Style
 import './css/Movie.css';
-import { Button, Form, InputGroup, Card } from 'react-bootstrap';
-import { Search } from 'react-bootstrap-icons';
-import { getMovieList, searchMovie } from "../Api";
+
+// React
 import { useEffect, useState } from "react";
+
+// React Bootstrap
+import { Button, Form, InputGroup, Card } from 'react-bootstrap';
+
+// React Bootstrap Icons
+import { Search } from 'react-bootstrap-icons';
+
+// Api
+import { getMovieList, searchMovie } from "../Api";
 
 const Movie = () => {
     const [popularMovies, setPopularMovies] = useState([]);
 
     useEffect(() => {
-      getMovieList().then((result) => {
-        setPopularMovies(result);
-      });
+        getMovieList().then((result) => setPopularMovies(result));
     }, []);
-  
+
     const PopularMovieList = () => {
-      return popularMovies.map((movie, i) => {
-        return (
-            <div className="col-md-3 d-flex justify-content-center" key={i}>
+        return popularMovies.map((item, index) => (
+            <div className="col-md-3 d-flex justify-content-center" key={index}>
                 <Card className="m-3" style={{ width: '13rem' }}>
-                    <Card.Img variant="top" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+                    <Card.Img variant="top" src={`${process.env.REACT_APP_BASEIMGURL}/${item.poster_path}`} />
                     <Card.Body>
-                        <Card.Title>{movie.title}</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Release: {movie.release_date}</Card.Subtitle>
-                        <Card.Subtitle className="text-muted">Rating: {movie.vote_average}</Card.Subtitle>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Release: {item.release_date}</Card.Subtitle>
+                        <Card.Subtitle className="text-muted">Rating: {item.vote_average}</Card.Subtitle>
                     </Card.Body>
                 </Card>
             </div>
-        );
-      });
+        ));
     };
-  
-    const search = async (q) => {
-      const query = await searchMovie(q);
-      setPopularMovies(query.results);
+
+    const handleSearchMovie = async (q) => {
+        const query = await searchMovie(q);
+        setPopularMovies(query.results);
     };
 
     return (
@@ -44,15 +49,8 @@ const Movie = () => {
                     </div>
                     <div className="col-md-4">
                         <InputGroup>
-                            <Form.Control
-                            placeholder="Search Movie"
-                            aria-label="Search Movie"
-                            aria-describedby="basic-addon2"
-                            onChange={({ target }) => search(target.value)}
-                            />
-                            <Button variant="secondary" id="button-addon2">
-                            <Search className="mb-1" />
-                            </Button>
+                            <Form.Control placeholder="Search Movie" onChange={(e) => handleSearchMovie(e.target.value)} />
+                            <Button variant="secondary"><Search className="mb-1" /></Button>
                         </InputGroup>
                     </div>
                 </div>
